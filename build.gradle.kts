14970ec21f9e586f5d227554d6db8432eacbd46a
Freitez93 <Freitez_93@hotmail.com>
2026-06-27 05:50:38 -0400

    ♻️ refactor: eliminar dependencias innecesarias y optimizar configuración de ProGuard en common.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
 buildscript {
     dependencies {
-        classpath(kotlin("stdlib"))
         classpath(libs.kotlin.gradle)
     }
 }
 
 plugins {
-    kotlin("jvm") version "2.2.0"
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.serialization) apply false
@@ -21,10 +19,4 @@ tasks {
             dependsOn(buildLogic.task(":$task"))
         }
     }
-}
-
-kotlin {
-    jvmToolchain(11)
-    sourceSets["main"].kotlin.srcDir("src/main/java")
-    sourceSets["test"].kotlin.srcDir("src/test/java")
 }
